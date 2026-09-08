f6bd83f6ae7cb669792cc3cf3c85fe75ceedac39
Hyeji Han <hj4094@gmail.com>
2026-09-09 01:08:11 +0900

    Feat: 음성 스트림 origin 제한 추가

--- src/main/java/com/silvertown/global/config/VoiceStreamWebSocketConfig.java
+++ src/main/java/com/silvertown/global/config/VoiceStreamWebSocketConfig.java
@@ -10,6 +10,8 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @Configuration
 @EnableWebSocket
 public class VoiceStreamWebSocketConfig implements WebSocketConfigurer {
+    private static final String FRONTEND_ORIGIN = "http://localhost:5173";
+
     private final VoiceStreamWebSocketHandler voiceStreamWebSocketHandler;
 
     public VoiceStreamWebSocketConfig(VoiceStreamWebSocketHandler voiceStreamWebSocketHandler) {
@@ -18,6 +20,7 @@ public class VoiceStreamWebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(voiceStreamWebSocketHandler, "/api/voice/sessions/*/stream");
+        registry.addHandler(voiceStreamWebSocketHandler, "/api/voice/sessions/*/stream")
+                .setAllowedOrigins(FRONTEND_ORIGIN);
     }
 }
